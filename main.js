7ff9a0749dcca4712c9648871d78f1f6825f3026
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-06-01 23:57:26 +0500

    added html in js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 
 
+const createEl = document.createElement('countryWrap')
+
 function getCountryInformation(country){
     //  Country AJAX CAll Variable
     const countryRequest = new XMLHttpRequest();
@@ -11,6 +13,18 @@ function getCountryInformation(country){
     countryRequest.addEventListener('load', (e) => {
         console.log(countryRequest.responseText)
     })
+
+    createEl.innerHTML = `
+    <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+      <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+      <button class="btn btn-primary btn-lg" type="button">Example button</button>
+    </div>
+  </div>
+    `
+
+    
 }
 
 getCountryInformation("india");
