f5b2bbb8b68b0b2835bed50cffc1e113d80ad10f
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-06-01 14:39:38 +0500

    Get data from AJAX call and show it on console.log

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+function getCountryInformation(country){
+    //  Country AJAX CAll Variable
+    const countryRequest = new XMLHttpRequest();
+    countryRequest.open('GET',  `https://restcountries.com/v3.1/name/${country}`);
+    //  Send Request to server (It will not get any data unless we use event load on it, because it is processing in background). It's a asynchronus request
+    countryRequest.send();
+    //  Using Load event to load country information
+    countryRequest.addEventListener('load', (e) => {
+        console.log(countryRequest.responseText)
+    })
+}
+
+getCountryInformation("india");
